d97a36fa9ecc18f16fd8b35fda560999af9fa498
Ratstsan Nur Akmal Adiwijaya <102446198+ratstsan@users.noreply.github.com>
2024-06-21 15:32:10 +0700

    Delete LatihanPropertyAnimationFinal directory

--- LatihanPropertyAnimationFinal/app/src/main/java/com/dicoding/picodiploma/loginwithanimation/data/pref/UserModel.kt
+++
@@ -1,9 +0,0 @@
-package com.dicoding.picodiploma.loginwithanimation.data.pref
-
-data class UserModel(
-    val userId: String,
-    val name: String,
-    val email: String,
-    val token: String,
-    val isLogin: Boolean = false
-)
